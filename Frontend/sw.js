197aef94951f356aabd259e9c35b1fc34711e075
Esther447 <e.mushimiyi@alustudent.com>
2025-07-16 01:15:14 +0200

    itabaza

--- Frontend/sw.js
+++ Frontend/sw.js
@@ -42,3 +42,11 @@ self.addEventListener("fetch", function (event) {
     return new Response("Offline or resource not available");
   }));
 });
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(() => console.log('SW registered'))
+    .catch(console.error);
+}
+
