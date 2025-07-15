874208e23599682dec985f47b06677fdec0b7de4
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-07-15 08:34:03 +0200

    iTABAZA

--- Backend/routers/appointment.router.js
+++ Backend/routers/appointment.router.js
@@ -137,7 +137,7 @@ appointmentRouter.post("/create/:doctorId", authenticate, async (req, res) => {
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: patientEmail,
-      subject: "Medistar Appointment Confirm",
+      subject: "iTABAZA Appointment Confirm",
       html: `
       <!DOCTYPE html>
         <html>
@@ -150,7 +150,7 @@ appointmentRouter.post("/create/:doctorId", authenticate, async (req, res) => {
             <table style="width: 100%; max-width: 600px; margin: 0 auto; background-color: #fff; border-collapse: collapse;">
               <tr>
                 <td style="background-color: #0077c0; text-align: center; padding: 10px;">
-                  <h1 style="font-size: 28px; color: #fff; margin: 0;">MEDISTAR HOSPITALS</h1>
+                  <h1 style="font-size: 28px; color: #fff; margin: 0;">iTABAZA</h1>
                 </td>
               </tr>
               <tr>
@@ -158,9 +158,9 @@ appointmentRouter.post("/create/:doctorId", authenticate, async (req, res) => {
                   <h2 style="font-size: 24px; color: #0077c0; margin-top: 0;">Hello, [${patientFirstName}]</h2>
                   <h5 style="margin-bottom: 20px;">Thank you for your recent appointment with ${docFirstName}. Your appointment has been booked for [${problemDescription}] on [${appointmentDate}]</h5>
                   <p style="margin-bottom: 20px;">If you do have any issues, please don't hesitate to contact our customer service team. We're always happy to help.</p>
-                  <p style="margin-bottom: 20px;">Thank you for choosing Medistar Services</p>
+                  <p style="margin-bottom: 20px;">Thank you for choosing iTABAZA Services</p>
                   <p style="margin-bottom: 0;">Best regards,</p>
-                  <p style="margin-bottom: 20px;">Medistar Hospitals</p>
+                  <p style="margin-bottom: 20px;">iTABAZA</p>
                 </td>
               </tr>
             </table>
